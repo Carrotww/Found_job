a6b571d88f47f394ad4e8e702972d48119aa5586
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-11-08 22:21:36 +0900

    commit

--- kakao_api.py
+++ kakao_api.py
@@ -1,2 +1,7 @@
+# 카카오톡 메시지
 # https://developers.kakao.com/docs/latest/ko/message/rest-api
+
+# 카카오톡 채널
+# https://developers.kakao.com/docs/latest/ko/kakaotalk-channel/common
+
 import Found_job.kakao_key
